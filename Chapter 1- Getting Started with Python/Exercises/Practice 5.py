aa66c6483579e706aeefedfcb6d0679ca8f265b4
Mohamed Shakin <m.shakin2005@gmail.com>
2023-10-09 19:38:29 +0400

    Update Practice 5.py

--- Chapter 1- Getting Started with Python/Exercises/Practice 5.py
+++ Chapter 1- Getting Started with Python/Exercises/Practice 5.py
@@ -1,2 +1,8 @@
 #Practice 5
 #Write four strings in different variables and print the output as one string. (String Concatenation)
+a="Bath "
+b="Spa "
+c="University "
+d="RAK"
+string=a+b+c+d
+print (string)
