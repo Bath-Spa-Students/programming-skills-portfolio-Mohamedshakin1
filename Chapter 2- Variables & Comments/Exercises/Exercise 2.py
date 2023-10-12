cd91fe7d7ef4f1f240fe71085b944c97d4a522e6
Mohamed Shakin <m.shakin2005@gmail.com>
2023-10-12 20:11:57 +0400

    Update Exercise 2.py

--- Chapter 2- Variables & Comments/Exercises/Exercise 2.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise 2.py
@@ -1,2 +1,7 @@
 #Exercise 2
+"""Find a quote from a famous person you admire. Print the quote and the name of its author. 
+Your output should look something like the following, including the quotation marks:
+Albert Einstein once said, “A person who never made a mistake never tried anything new."""
+
+#Answer
 print ('''Sheikh Mohammed once said, “When you do what you like, you won't feel like you are doing work.''')
