a5ddd092961f1d3f1b2518661c62c6c8f5f335a0
Mohamed Shakin <m.shakin2005@gmail.com>
2023-10-12 09:32:16 +0400

    Update Exercise 2.py

--- Chapter 2- Variables & Comments/Exercises/Exercise 2.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise 2.py
@@ -1,2 +1,2 @@
 #Exercise 2
-print ('''Sheikh Mohammed once said, “When you do what you like, you won't feel like you are doing work."''')
+print ('''Sheikh Mohammed once said, “When you do what you like, you won't feel like you are doing work.''')
