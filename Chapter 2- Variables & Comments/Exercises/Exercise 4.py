1464fd629dc1304c66805eece1eaab2ad94b449b
Mohamed Shakin <m.shakin2005@gmail.com>
2023-10-12 20:09:18 +0400

    Update Exercise 4.py

--- Chapter 2- Variables & Comments/Exercises/Exercise 4.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise 4.py
@@ -1,3 +1,7 @@
 #Exercise 4
-number=int(input("Enter your favourite number: "))
+'''Use a variable to represent your favorite number. Then,using that variable, create a message that reveals your favorite number. Print
+that message.'''
+
+#Answer
+number=7
 print ("Your favourite number is",number)
