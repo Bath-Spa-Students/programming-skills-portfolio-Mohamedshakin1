5aee6414c50dba0177248b4c4d601cb6aed64277
Mohamed Shakin <m.shakin2005@gmail.com>
2023-10-12 20:12:04 +0400

    Update Exercise 1.py

--- Chapter 2- Variables & Comments/Exercises/Exercise 1.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise 1.py
@@ -1,4 +1,9 @@
 #Exercise 1
+"""Assign a message to a variable, and print that message.
+Then change the value of the variable to a new message, and print the new
+message."""
+
+#Answer
 message="Good Morning"
 print(message)
 message="Good Night"
