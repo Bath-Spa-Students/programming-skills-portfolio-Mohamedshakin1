5ba97f8221f67780f38d2c2bed2e2392cae637fc
Mohamed Shakin <m.shakin2005@gmail.com>
2023-10-27 15:57:36 +0400

    Update Exercise 3.py

--- Chapter 4- Control Flow/Exercises/Exercise 3.py
+++ Chapter 4- Control Flow/Exercises/Exercise 3.py
@@ -21,6 +21,18 @@ else:
 
 #Second version
 alien_color='green'
+if alien_color=='green':
+    print ("Player earned 5 points")
+elif alien_color=='yellow':
+    print ("Player earned 10 points")
+else:
+    print ("Player earned 15 points")
 
 #Third version
 alien_color="red"
+if alien_color=='green':
+    print ("Player earned 5 points")
+elif alien_color=='yellow':
+    print ("Player earned 10 points")
+else:
+    print ("Player earned 15 points")
