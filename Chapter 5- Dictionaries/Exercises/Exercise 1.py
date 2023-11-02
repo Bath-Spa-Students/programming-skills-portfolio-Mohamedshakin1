722db381bfe3cbe72423d25f929022863523acf4
Mohamed Shakin <m.shakin2005@gmail.com>
2023-11-02 20:54:49 +0400

    Exercise 1.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise 1.py
@@ -0,0 +1,10 @@
+#Exercise 1
+"""Use a details to store information about a person you know.Store their first name, last name, age, and the city in which they live. 
+You should have keys such as first_name, last_name, age, and city. Print each piece of information stored in your details."""
+
+#Answer
+details={'first_name': 'Mohamed', 'last_name': 'Azfar', 'age': 17, 'city': 'Sharjah'}
+print (details['first_name'])
+print (details['last_name'])
+print (details['age'])
+print (details['city'])
