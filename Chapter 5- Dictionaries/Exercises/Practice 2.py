e20fa37588ddc68f10b486272da362a01845a48c
Mohamed Shakin <m.shakin2005@gmail.com>
2023-11-07 22:14:07 +0400

    Practice 2.py

---
+++ Chapter 5- Dictionaries/Exercises/Practice 2.py
@@ -0,0 +1,11 @@
+#Practice 2
+#Write a Python program to iterate through the keys of a dictionary and print them.
+
+#Answer
+dict_1={'first_name':'Mohamed',
+        'last_name':'Shakin',
+        'age':18}
+
+#iterate through the keys of a dictionary and print them
+for i in dict_1.keys():
+    print (i)
