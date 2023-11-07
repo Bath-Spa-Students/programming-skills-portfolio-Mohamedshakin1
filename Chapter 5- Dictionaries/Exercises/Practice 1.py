029ace763ceceb25adbf6992a967693f80ba1537
Mohamed Shakin <m.shakin2005@gmail.com>
2023-11-07 22:13:54 +0400

    Practice 1.py

---
+++ Chapter 5- Dictionaries/Exercises/Practice 1.py
@@ -0,0 +1,10 @@
+#Practice 1
+#Use a dictionary to store information about yourself
+
+#Answer
+
+#store information about yourself
+dict_1={'first_name':'Mohamed',
+        'last_name':'Shakin',
+        'age':18}
+print (dict_1)
