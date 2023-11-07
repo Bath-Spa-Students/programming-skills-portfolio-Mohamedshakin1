bc2bf14e197728aa4c820c1fc5519340fe1da012
Mohamed Shakin <m.shakin2005@gmail.com>
2023-11-07 22:09:27 +0400

    Update Practice 3.py

--- Chapter 1- Getting Started with Python/Exercises/Practice 3.py
+++ Chapter 1- Getting Started with Python/Exercises/Practice 3.py
@@ -1,5 +1,8 @@
 #Practice 3
 #You need to make a program for a leader board. The program needs to output the numbers 1 to 9, each on a separate line followed by a dot.
+
+#Answer
+#pair of triple quotation mark is used to print in multiple lines
 print ('''1.
 2.
 3.
