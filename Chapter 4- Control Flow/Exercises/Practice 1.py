adacd6584f6af43a68431e4a21c2cb9136bb3ac0
Mohamed Shakin <m.shakin2005@gmail.com>
2023-11-07 22:12:30 +0400

    Practice 1.py

---
+++ Chapter 4- Control Flow/Exercises/Practice 1.py
@@ -0,0 +1,15 @@
+#Practice 1
+"""Write a python program with if statement that assigns 20 to the variable y and assigns 40 to the
+variable z if the variable x is greater than 100."""
+
+#Answer
+x=int(input("Enter a number= "))
+#if x is greater than 100 than it assigns value 20 to the variable y, and it assigns value 40 to the variable z
+#no output, if x is not greater than 10
+
+if x>100:
+    y=20
+    z=40
+
+print ("y=",y)
+print ("z=",z)
