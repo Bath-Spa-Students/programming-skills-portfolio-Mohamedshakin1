82e02ec33665ac44df7617e6ee13cdc311af44f0
Mohamed Shakin <m.shakin2005@gmail.com>
2023-11-07 22:14:28 +0400

    Practice 4.py

---
+++ Chapter 5- Dictionaries/Exercises/Practice 4.py
@@ -0,0 +1,11 @@
+#Practice 4
+#Write a Python program to iterate through both the keys and values of a dictionary and print them.
+
+#Answer
+dict_1={'first_name':'Mohamed',
+        'last_name':'Shakin',
+        'age':18}
+
+#iterate through both the keys and values of a dictionary and print them.
+for i in dict_1.items():
+    print (i)
