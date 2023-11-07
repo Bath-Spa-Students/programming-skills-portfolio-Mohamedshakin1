8cb790fbdd75e57f30d46adadac6a1f148b5dfba
Mohamed Shakin <m.shakin2005@gmail.com>
2023-11-07 22:11:45 +0400

    Practice 3.py

---
+++ Chapter 3- Data Structures/Exercises/Practice 3.py
@@ -0,0 +1,8 @@
+#Practice 3
+#Assume names references a list. Write a for loop that displays each element of the list.
+
+#Answer
+names=['Abubakar','Isaac','Adil','Shakin','Rashid']
+#for loop that displays each element of the list
+for i in names:
+    print (i)
