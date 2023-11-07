f0cea9436cee4cf29af21220bc214f16840f7189
Mohamed Shakin <m.shakin2005@gmail.com>
2023-11-07 22:10:07 +0400

    Practice 1.py

---
+++ Chapter 2- Variables & Comments/Exercises/Practice 1.py
@@ -0,0 +1,10 @@
+#Practice 1
+"""Write a python code that uses three variables x, y, and z with respective values 10,20 and 30. 
+Add all the variables and then store the result in variable a. Print the value of a."""
+
+#Answer
+x=10
+y=20
+z=30
+a=x+y+z #Adding all numbers
+print (a)
