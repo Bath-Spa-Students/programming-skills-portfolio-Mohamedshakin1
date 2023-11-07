de71a73363f6c32aae344fd3d0508c3cf7da5ca7
Mohamed Shakin <m.shakin2005@gmail.com>
2023-11-07 22:09:31 +0400

    Update Practice 4.py

--- Chapter 1- Getting Started with Python/Exercises/Practice 4.py
+++ Chapter 1- Getting Started with Python/Exercises/Practice 4.py
@@ -2,5 +2,7 @@
 #Compute the area of triangle.
 h=float(input("enter the height of a trangle= "))
 b=float(input("enter the base of a triangle= "))
+
+#area of triangle is 1/2 x height x breadth
 area=1/2*h*b
-print ("area of triangle is",area,"cm square")
+print ("area of triangle is",area,"cm square") #Area is measured in cm square
