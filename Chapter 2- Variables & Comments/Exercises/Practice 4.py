ba62ae12e377e9b3fbc59470e6e09a6e3b7a4b3e
Mohamed Shakin <m.shakin2005@gmail.com>
2023-11-07 22:10:40 +0400

    Practice 4.py

---
+++ Chapter 2- Variables & Comments/Exercises/Practice 4.py
@@ -0,0 +1,10 @@
+#Practice 4
+"""Write a python program that stores an integer and string value to variables x and y. Print the type of
+variable x and y. """
+
+#Answer
+x= int(input("Enter any number= "))
+y=input("Enter any data= ")
+
+print ('x is',type(x)) #Displays the type of variable x
+print ('y is',type(y)) #Displays the type of variable y
