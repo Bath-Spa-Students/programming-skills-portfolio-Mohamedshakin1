00b6d67031a8ba9f68897db38cb92d463d1b9ca2
Mohamed Shakin <m.shakin2005@gmail.com>
2023-11-07 22:11:05 +0400

    Update Exercise 4.py

--- Chapter 3- Data Structures/Exercises/Exercise 4.py
+++ Chapter 3- Data Structures/Exercises/Exercise 4.py
@@ -4,9 +4,9 @@ you’dlike to invite to dinner. Then use your list to print a message to each p
 
 #Answer
 names=["Azfar","Aseel","Suhail","Saif","Maahir","Tufail"]
-print (names[0]+", I am inviting you to dinner")
-print (names[1]+", I am inviting you to dinner")
-print (names[2]+", I am inviting you to dinner")
-print (names[3]+", I am inviting you to dinner")
-print (names[4]+", I am inviting you to dinner")
-print (names[5]+", I am inviting you to dinner")
+print (names[0]+", I am inviting you to dinner") #Message to first person of the list
+print (names[1]+", I am inviting you to dinner") #Message to second person of the list
+print (names[2]+", I am inviting you to dinner") #Message to third person of the list
+print (names[3]+", I am inviting you to dinner") #Message to fourth person of the list
+print (names[4]+", I am inviting you to dinner") #Message to fifth person of the list
+print (names[5]+", I am inviting you to dinner") #Message to sixth person of the list
