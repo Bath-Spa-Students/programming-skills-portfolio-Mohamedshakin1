a41c4cf788695625be5c0f7084e1a4e593e56b8e
Mohamed Shakin <m.shakin2005@gmail.com>
2023-11-07 22:14:17 +0400

    Practice 3.py

---
+++ Chapter 5- Dictionaries/Exercises/Practice 3.py
@@ -0,0 +1,11 @@
+#Practice 3
+#Write a Python program to iterate through the values of a dictionary and print them.
+
+#Answer
+dict_1={'first_name':'Mohamed',
+        'last_name':'Shakin',
+        'age':18}
+
+#iterate through the values of a dictionary and print them
+for i in dict_1.values():
+    print (i)
