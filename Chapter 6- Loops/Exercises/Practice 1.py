eb35ed810b2eaaf87e867de20eee0f8c6924931d
Mohamed Shakin <m.shakin2005@gmail.com>
2023-11-10 15:37:01 +0400

    Practice 1.py

---
+++ Chapter 6- Loops/Exercises/Practice 1.py
@@ -0,0 +1,11 @@
+#Practice 1
+#Write a Python program that uses a while loop to print the even numbers from 2 to 10
+
+#Answer
+
+#2 is an initial value
+i=2
+#loop will run till i is less or equal to 10, else loop ends
+while i<=10:
+    print (i)
+    i+=2 #2 is an increment or adding 2 to the i in each loop.
