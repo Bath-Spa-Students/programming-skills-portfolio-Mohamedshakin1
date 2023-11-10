cf8ecfad6ba1778a5e7d0d11ec8e38f23d12e1d4
Mohamed Shakin <m.shakin2005@gmail.com>
2023-11-10 15:37:41 +0400

    Practice 4.py

---
+++ Chapter 6- Loops/Exercises/Practice 4.py
@@ -0,0 +1,14 @@
+#Practice 4
+#Write a Python program that uses the break statement to exit a for loop when a specific condition is met.
+
+#Answer
+list1=[]
+while True:
+    name=input("Enter a name: ")
+    # if name is equal to exit then loop ends, else loop will continue adding names in list.
+    if name.lower()=="exit":
+        break
+    else:
+        list1.append(name)
+        print (list1)
+
