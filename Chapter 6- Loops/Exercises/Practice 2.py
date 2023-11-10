efdab3cabe72bbcc782753f792b645c9bcbf5530
Mohamed Shakin <m.shakin2005@gmail.com>
2023-11-10 15:37:15 +0400

    Practice 2.py

---
+++ Chapter 6- Loops/Exercises/Practice 2.py
@@ -0,0 +1,8 @@
+#Practice 2
+#Write a Python program that uses a for loop to print the numbers from 1 to 10.
+
+#Answer
+
+#1 is an initial value and 10 is a final value.
+for i in range (1,11):
+    print (i)
