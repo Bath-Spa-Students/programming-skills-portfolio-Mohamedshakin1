4985a761b9b7c771156641a1c666dafb1cc4ce24
Mohamed Shakin <m.shakin2005@gmail.com>
2023-11-10 15:33:55 +0400

    Exercise 3.py

---
+++ Chapter 6- Loops/Exercises/Exercise 3.py
@@ -0,0 +1,8 @@
+#Exercise 3
+#Write a loop that never ends, and run it. (To end the loop, press ctrl-C or close the window displaying the output.)
+
+#Answer
+number=1
+#number==1 is true, the loop will continue running as long as the 'True' condition is met
+while number==1:
+    print ("It's Okay")
