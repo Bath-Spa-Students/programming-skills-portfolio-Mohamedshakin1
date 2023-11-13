bbe4f30d7da6b11fa4d3dc3459c5263b469ae153
Mohamed Shakin <m.shakin2005@gmail.com>
2023-11-13 18:55:39 +0400

    Exercise 1.py

---
+++ Chapter 7- Functions/Exercises/Exercise 1.py
@@ -0,0 +1,11 @@
+#Exercise 1
+"""Write a function called display_message() that prints one sentence telling everyone what you are learning about in this chapter. 
+Call the function, and make sure the message displays correctly."""
+
+#Answer
+#Creating the fuction called display_message 
+def display_message():
+    print ("we are learning about function in this chapter")  #prints message
+
+#Calling the function
+display_message()
