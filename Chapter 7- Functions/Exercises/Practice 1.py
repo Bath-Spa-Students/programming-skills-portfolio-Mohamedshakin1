418ddc05b9d570a1e190cc8376595c9d5f47751b
Mohamed Shakin <m.shakin2005@gmail.com>
2023-11-13 18:56:38 +0400

    Practice 1.py

---
+++ Chapter 7- Functions/Exercises/Practice 1.py
@@ -0,0 +1,12 @@
+#Practice 1
+#Write a Python function that takes two numbers as arguments and returns their sum.
+
+#Answer
+
+#Creating the fuction called sum that accepts a num1 and num2.
+def sum(num1,num2):
+    print ("sum of two number=",num1+num2) #print the sum of two numbers
+
+#Calling the function
+#it takes 1 as first argument and it takes 2 as second argurment.
+sum(1,2)
